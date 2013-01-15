5a9d4015c6fc969e6329f8957c1db56c3fee8bc2
Patrick Gunderson <patrick.gunderson@b-reel.com>
2013-01-15 14:29:22 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,126 @@
+# jQuery.SpriteAnimation
+
+Makes a div element and animates through a sequence of frames in a single image.
+
+## Getting Started
+Download the [production version][min] or the [development version][max].
+
+[min]: https://raw.github.com/patrickgunderson/jQuery.spriteAnimation/master/dist/jQuery.spriteAnimation.min.js
+[max]: https://raw.github.com/patrickgunderson/jQuery.spriteAnimation/master/dist/jQuery.spriteAnimation.js
+
+In your web page:
+
+```html
+<script src="jquery.js"></script>
+<script src="dist/jQuery.spriteAnimation.min.js"></script>
+<script>
+$(".testButton").spriteAnimation({
+    src: "filmstrip.png",
+    frameWidth: 24,
+    frameHeight: 24,
+    frameSpacing: 1,
+    frameRate: 60,
+    numFrames: 11
+  })
+</script>
+```
+
+## Documentation
+
+### Methods
+
+#### play
+
+    $(".testButton").spriteAnimation('play');
+
+Plays the animation sequence forward.
+
+#### rewind
+
+    $(".testButton").spriteAnimation('rewind');
+
+Plays the animation sequence backward.
+
+#### stop
+
+    $(".testButton").spriteAnimation('stop');
+
+Stops the animation sequence  where it is.
+
+#### reset
+
+    $(".testButton").spriteAnimation('reset');
+
+Stops the animation sequence and frame to 0.
+
+### Options
+
+#### src - Required
+
+Image source url.
+
+    { src: "filmstrip.png" }
+
+
+#### firstFramePosition
+
+Position of the first frame in sequence. (allows you to put multiple sequences in one image)
+
+    // Object with x and y parameters. 
+    { firstFramePosition: {x:0, y:0} }
+
+#### orientation
+
+Position of the first frame in sequence. (allows you to put multiple sequences in one image)
+
+    // String, either 'x' or 'y'. 
+    { orientation: "x" }
+
+#### frameWidth
+
+Width of the frame in px
+
+    // Number 
+    { frameWidth: 25 }
+
+#### frameHeight
+
+Height of the frame in px
+
+    // Number 
+    { frameHeight: 25 }
+
+#### frameSpacing
+
+Spacing between frames in px
+
+    // Number 
+    { frameSpacing: 0 }
+
+#### frameRate
+
+Frames per second
+
+    // Number 
+    { frameRate: 25 }
+
+#### numFrames
+
+Number of frames in the sequence.
+
+    // Number 
+    { numFrames: 10 }
+
+#### loop
+
+Loop on complete.
+
+    // Boolean 
+    { loop: false }
+
+#### onComplete
+
+Callback on complete.
+
+    // Function 
+    { onComplete: null }
---
+++ src/jQuery.spriteAnimation.js
@@ -0,0 +1,212 @@
+/*
+ * jQuery.spriteAnimation
+ * https://github.com/patrickgunderson/jQuery.spriteAnimation
+ *
+ * Copyright (c) 2013 Patrick Gunderson
+ * Licensed under the MIT license.
+ */
+
+ (function($) {
+    $.spriteAnimation = function(el, options){
+        // To avoid scope issues, use 'base' instead of 'this'
+        // to reference this class from internal events and functions.
+        var base = this;
+        
+        // Access to jQuery and DOM versions of element
+        base.$el = $(el);
+        base.el = el;
+        base.currentFrame = 0;
+        base.running = false;
+        base.timeout = null;
+        
+        // Add a reverse reference to the DOM object
+        base.$el.data("spriteAnimation", base);
+        
+        base.init = function(){
+            base.el = el;
+            base.options = $.extend({},$.spriteAnimation.options, options);
+            base.$el = $(this.el);
+            base.generate();
+            base.setFrameRate(base.options.frameRate);
+        };
+
+        base.generate = function(){
+            base.$anim = $("<div/>").addClass('spriteAnimation');
+            base.$anim.css({
+              backgroundImage: 'url(' + base.options.src + ')',
+              backgroundPosition: base.options.firstFramePosition.x + "px " + base.options.firstFramePosition.y + "px",
+              width: (base.options.frameWidth) + "px",
+              height: (base.options.frameHeight) + "px",
+              overflow: "hidden"
+          });
+            base.$el.append(base.$anim);
+        };
+
+        base.setFrameRate = function(frameRate){
+            base.frameRate = frameRate;
+            base.frameDelay = 1000 / frameRate;
+            return base;
+        };
+
+        base.play = function(){
+            base.running = true;
+            base.options.reverse = false;
+            base.delayNextFrame();
+            return base;
+        };
+
+        base.rewind = function(){
+            base.running = true;
+            base.options.reverse = true;
+            base.delayPrevFrame();
+            return base;
+        };
+
+        base.stop = function(){
+            if (base.timeout) clearTimeout(base.timeout);
+            base.running = false;
+            return base;
+        };
+
+        base.reset = function(){
+            base.stop();
+            base.currentFrame = 0;
+            base.gotoFrame(0);
+            return base;
+        };
+
+        base.gotoFrame = function(frameNumber){
+            var newX = -base.options.firstFramePosition.x;
+            if (base.options.orientation === "x" ) {
+                newX = (-frameNumber * (base.options.frameWidth + base.options.frameSpacing)) - base.options.firstFramePosition.x;
+            }
+            var newY = -base.options.firstFramePosition.y;
+            if (base.options.orientation === "y" ) {
+                newY = (-frameNumber * (base.options.frameWidth + base.options.frameSpacing)) - base.options.firstFramePosition.y;
+            }
+
+            base.$anim.css({
+                backgroundPosition: newX + "px " + newY + "px"
+            });
+            if (base.options.reverse){
+                base.delayPrevFrame();
+            } else {
+                base.delayNextFrame();
+            }
+            return base;
+        };
+
+        base.nextFrame = function(){
+            if (++base.currentFrame < base.options.numFrames){
+                return base.gotoFrame(base.currentFrame);
+            } else {
+                if (base.options.loop){
+                    base.currentFrame = 0;
+                    base.$el.trigger('loop');
+                    return gotoFrame(base.currentFrame);
+                }
+            }
+        };
+
+        base.prevFrame = function(){
+            if (--base.currentFrame >= 0){
+                return base.gotoFrame(base.currentFrame);
+            } else {
+                if (base.options.loop){
+                    base.currentFrame = base.options.numFrames;
+                    base.$el.trigger('loop');
+                    return gotoFrame(base.currentFrame);
+                }
+            }
+        };
+
+        base.delayNextFrame = function(){
+            if (base.running){
+                base.timeout = setTimeout(function() {
+                    base.nextFrame();
+                }, base.frameDelay);
+            }
+        };
+
+
+        base.delayPrevFrame = function(){
+            if (base.running){
+                base.timeout = setTimeout(function() {
+                    base.prevFrame();
+                }, base.frameDelay);
+            }
+        };
+
+        base.complete = function(){
+            if (typeof(base.options.onComplete) === "function"){
+                base.options.onComplete();
+            }
+            base.$el.trigger('complete');
+        };
+
+        // Run initializer
+        base.init();
+        return base;
+    };
+
+    $.spriteAnimation.options =  {
+        src: "",
+        firstFramePosition: {x:0, y:0},
+        orientation: "x",
+        frameWidth: 25,
+        frameHeight: 25,
+        frameSpacing: 0,
+        frameRate: 25, // fps,
+        numFrames: 10,
+        loop: false,
+        reverse: false,
+        onComplete: null
+    };
+
+    // Collection method.
+    $.fn.spriteAnimation = function(settings) {
+        return this.each(function() {
+            var options, carousel;
+            if(!(anim = this.spriteAnimation)) {
+                if (!settings.src) {
+                    if (!(settings.src = this.css('backgroundImage')) ||
+                        !(settings.src = this.attr('src'))) {
+                        var error = (console) ? console.error("SpriteAnimation requires image src.") : false;
+                        return this;
+                    }
+                }
+
+                options = $.extend({}, $.spriteAnimation.options, settings);
+                this.spriteAnimation = anim = new $.spriteAnimation(this, options);
+                var message = (console) ? console.log("new SpriteAnimation, " + settings.src): true;
+            }
+            if(settings && typeof settings === "object") {
+                $.extend(anim.options, settings);
+                if (settings.frameRate){
+                    anim.setFrameRate(settings.frameRate);
+                }
+                if (settings.frame) {
+                    anim.gotoFrame(settings.frame);
+                }
+            } else if(settings && typeof settings === "string") {
+                if(settings === "play") {
+                    anim.play();
+                }
+                if(settings === "rewind") {
+                    anim.rewind();
+                }
+                if(settings === "stop") {
+                    anim.stop();
+                }
+                if(settings === "reset") {
+                    anim.reset();
+                }
+            } else if(settings && !isNaN( parseInt(settings, 10) )){
+                anim.gotoFrame(settings);
+            } else if (!settings){
+                anim.reset();
+            }
+            return this;
+        });
+    };
+})(jQuery);
